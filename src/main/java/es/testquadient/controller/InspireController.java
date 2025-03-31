c164b040ccf19bcd24516b5f58f94c3043b1afee
pablogonzalezamores <pablo.gonzalezamores@plexus.es>
2025-03-31 15:49:29 +0200

    importacion corregida

--- src/main/java/es/testquadient/controller/InspireController.java
+++ src/main/java/es/testquadient/controller/InspireController.java
@@ -1,7 +1,7 @@
 package es.testquadient.controller;
 
 import es.testquadient.service.InspireService;
-import jakarta.annotation.Resource;
+import org.springframework.core.io.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
