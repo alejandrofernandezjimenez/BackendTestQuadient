fc738786a2d38bc5f74c27c1987a73c0cb4f95df
alejandrofernandezjimenez <alejandro.fernandezjimenez@plexus.es>
2025-03-31 15:39:16 +0200

    Drop download pdf

--- src/main/java/es/testquadient/controller/InspireController.java
+++ src/main/java/es/testquadient/controller/InspireController.java
@@ -1,9 +1,18 @@
 package es.testquadient.controller;
 
 import es.testquadient.service.InspireService;
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+
+
+import java.io.File;
+import java.io.FileInputStream;
 
 @RestController
 public class InspireController {
@@ -15,4 +24,28 @@ public class InspireController {
     public String generarPDF() {
         return inspireService.ejecutarInspire();
     }
+
+    @GetMapping("/descargar-pdf")
+    public ResponseEntity<Resource> descargarPDF() {
+        try {
+            String userHome = System.getProperty("user.home");
+            File archivo = new File(userHome + "\\Downloads\\Output1.pdf");
+
+            if (!archivo.exists()) {
+                return ResponseEntity.notFound().build();
+            }
+
+            InputStreamResource resource = new InputStreamResource(new FileInputStream(archivo));
+
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=Output1.pdf")
+                    .contentLength(archivo.length())
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .body((Resource) resource);
+
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
 }
